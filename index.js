12cd1a2660971f74ef30b035681aa3ed744c3993
一号同学 <1009183116@qq.com>
2020-12-30 23:35:46 +0800

    新增

--- index.js
+++ index.js
@@ -9,17 +9,67 @@ const config = {
   Region: 'ap-chengdu',
 };
 
+document.addEventListener('click', () => {
+  document.getElementById('menuID')?.remove();
+})
 
-cos.getBucket(config, (err, data)=> {
-  if (err) {
-    return;
-  }
-  const content = document.getElementById('container');
-  data.Contents.forEach(item => {
-    const child = renderImage(imgBaseURL + item.Key);
-    content.appendChild(child);
-  })
-});
+document.body.onload = () => {
+  getCOSBucket();
+}
+
+function handleChange(data) {
+  const load = renderLoading();
+  document.body.appendChild(load);
+  getFiles(data.files).forEach((file, index, arr) => {
+    const key = file.name;
+    if(!['image/jpeg', 'image/png', 'video/mp4', 'image/gif'].includes(file.type)) {
+      return;
+    }
+    putCOSObject({
+      key,
+      body: file,
+      onProgress: e => {
+        load.innerHTML = '已上传' + parseInt(e.percent * 100 * index / arr.length) + '%';
+      },
+      onSuccess: () => {
+        load.remove();
+        document.getElementById('content').appendChild(renderImage(imgBaseURL + key));
+      },
+      onError: () => { },
+    });
+  });
+}
+
+function handleDelete(url) {
+  const load = renderLoading();
+  document.body.appendChild(load);
+  deleteCOSObject({
+    key: url.slice(imgBaseURL.length - 1),
+    onSuccess: () => {
+      document.querySelectorAll('img').forEach(img => {        
+        img.src === encodeURI(url) && img.parentElement.remove();
+      });
+      load.remove();
+    },
+    onError: err => console.log(err)
+  });
+}
+
+function getCOSBucket() {
+  const load = renderLoading();
+  document.body.appendChild(load);
+  cos.getBucket(config, (err, data) => {
+    if (err) {
+      return;
+    }
+    const content = document.getElementById('content');
+    data.Contents.forEach(item => {
+      const child = renderImage(imgBaseURL + item.Key);
+      content.appendChild(child);
+    })
+    load.remove();
+  });
+}
 
 function putCOSObject({
   key: Key,
@@ -39,8 +89,66 @@ function putCOSObject({
   });
 }
 
+function deleteCOSObject({
+  key: Key,
+  onError,
+  onSuccess,
+}) {
+  cos.deleteObject({
+    ...config,
+    Key
+  }, (err, data) => {
+    err ? onError(err) : onSuccess(data);
+  });
+}
+
 function renderImage(url) {
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+  p.innerHTML = url.slice(url.lastIndexOf('/') + 1);
   const img = new Image();
   img.src = url;
-  return img;
+  img.style.backgroundColor = `rgba(${random(100, 200)}, ${random(100, 200)}, ${random(100, 200)}, .8)`;
+  div.oncontextmenu = e => {
+    e.preventDefault();
+    document.getElementById('menuID')?.remove();
+    document.body.appendChild(renderMenu(url, e.pageX, e.pageY));
+  }
+  div.appendChild(img);
+  div.appendChild(p);
+  return div;
+}
+
+function renderMenu(url, x, y) {
+  const menuDOM = document.createElement('div');
+  menuDOM.id = 'menuID';
+  menuDOM.setAttribute('class', 'container-menu');
+  menuDOM.setAttribute('style', `left: ${x}px;top: ${y}px`);
+  menuDOM.oncontextmenu = e => e.preventDefault();
+  const deleteBtn = document.createElement('span');
+  deleteBtn.innerHTML = '删除';
+  deleteBtn.onclick = () => {
+    menuDOM.remove();
+    handleDelete(url)
+  };
+  menuDOM.appendChild(deleteBtn);
+  return menuDOM;
+}
+
+function renderLoading() {
+  const loadDOM = document.createElement('div');
+  loadDOM.innerHTML = '加载中...'
+  loadDOM.setAttribute('class', 'container-loading');
+  setTimeout(() => {
+    loadDOM.remove();
+  }, 20000);
+  return loadDOM;
+}
+
+function getFiles(files) {
+  return [...new Array(files.length)].map((v, i) => files.item(i));
+}
+
+function random(min, max) {
+  return parseInt(Math.random() * max + min);
 }
